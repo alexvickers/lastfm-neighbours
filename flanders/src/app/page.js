58b1ea50f599e24550d0eb772bcfefbc3ec58a56
Alexandre Aimbiré <alexvickers@gmail.com>
2025-05-18 22:24:34 -0300

    Kickstart My Heart

---
+++ flanders/src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Find My Neighbours</h1>
+      <p className="mt-4 text-gray-600">
+        A Last.fm-powered social discovery tool.
+      </p>
+    </main>
+  );
+}
