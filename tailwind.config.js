8d17e7f6962e576e82a35bbff8f6a40e6016415c
Alexandre Aimbiré <alexvickers@gmail.com>
2025-05-19 21:23:44 -0300

    Fixed issue with PostCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-// tailwind.config.js
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
